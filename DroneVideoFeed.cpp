0d862031b8d535def5cb7cab8d9430652410762e
Pavan Kumar B N <pavanbn8@gmail.com>
2019-04-23 20:10:21 +0900

    Create DroneVideoFeed.cpp

---
+++ DroneVideoFeed.cpp
@@ -0,0 +1,48 @@
+#include "opencv2/opencv.hpp"
+#include "opencv2/opencv_modules.hpp"
+#include "opencv2/highgui.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char* argv[]) {
+
+	// Create a VideoCapture object and open the input file
+	// If the input is the web camera, pass 0 instead of the video file name
+	VideoCapture cap("rtmp://192.168.0.100/live");
+
+	// Check if camera opened successfully
+	if (!cap.isOpened()) {
+		cout << "Error opening video stream or file" << endl;
+		return -1;
+	}
+
+	//setOpenGlDrawCallback("Frame", on_opengl);
+	while (1) {
+
+		Mat frame;
+		// Capture frame-by-frame
+		cap >> frame;
+
+		// If the frame is empty, break immediately
+		if (frame.empty())
+			break;
+
+		// Display the resulting frame
+		imshow("Frame", frame);
+
+		// Press  ESC on keyboard to exit
+		char c = (char)waitKey(25);
+		if (c == 27)
+			break;
+	}
+
+	// When everything done, release the video capture object
+	cap.release();
+
+	// Closes all the frames
+	destroyAllWindows();
+
+	return 0;
+}
